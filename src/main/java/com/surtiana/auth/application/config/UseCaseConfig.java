4fdf630cf0b82b85e6d5bad44e796a5cb0142108
Joseph Paredes <alejitoparedes2006@gmail.com>
2026-05-27 14:34:49 -0500

    Agrego la configuracion de use case

--- src/main/java/com/surtiana/auth/application/config/UseCaseConfig.java
+++ src/main/java/com/surtiana/auth/application/config/UseCaseConfig.java
@@ -1,5 +1,16 @@
 package com.surtiana.auth.application.config;
 
+import com.surtiana.auth.domain.model.gateway.EncrypterGateway;
+import com.surtiana.auth.domain.model.gateway.UsuarioGateway;
+import com.surtiana.auth.domain.usecase.UsuarioUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UseCaseConfig {
+    @Bean
+    public UsuarioUseCase usuarioUseCase (UsuarioGateway usuarioGateway, EncrypterGateway encrypterGateway){
+        return new UsuarioUseCase(usuarioGateway, encrypterGateway);
+    }
 
 }
