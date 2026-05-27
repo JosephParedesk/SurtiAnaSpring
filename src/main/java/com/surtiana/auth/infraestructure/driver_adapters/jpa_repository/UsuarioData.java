c4ad0136cbf6daef2c73ba8a2da97f97846e4162
Joseph Paredes <alejitoparedes2006@gmail.com>
2026-05-27 14:39:22 -0500

    Creacion entidad base de datos

---
+++ src/main/java/com/surtiana/auth/infraestructure/driver_adapters/jpa_repository/UsuarioData.java
@@ -0,0 +1,30 @@
+package com.surtiana.auth.infraestructure.driver_adapters.jpa_repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "usuarios")
+@Data
+
+public class UsuarioData {
+
+    @Id
+    private String cedula;
+    private String nombre;
+
+    @Column(length = 30, nullable = false)
+    private String correo;
+    private String contrasena;
+
+    @Column(length = 10)
+    private String telefono;
+    private String rol;
+}
