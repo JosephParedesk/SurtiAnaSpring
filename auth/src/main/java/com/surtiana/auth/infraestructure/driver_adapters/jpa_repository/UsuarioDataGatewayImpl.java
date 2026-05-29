74a63e391e0a106218cc7272d4a312435ae8b49d
Joseph Paredes <alejitoparedes2006@gmail.com>
2026-05-28 20:42:56 -0500

    Se agrego la implementacion a la BD y las consultas a la BD

---
+++ auth/src/main/java/com/surtiana/auth/infraestructure/driver_adapters/jpa_repository/UsuarioDataGatewayImpl.java
@@ -0,0 +1,46 @@
+package com.surtiana.auth.infraestructure.driver_adapters.jpa_repository;
+
+import com.surtiana.auth.domain.model.Usuario;
+import com.surtiana.auth.domain.model.gateway.UsuarioGateway;
+import com.surtiana.auth.infraestructure.mapper.UsuarioMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class UsuarioDataGatewayImpl implements UsuarioGateway {
+
+    private final UsuarioDataJpaRepository usuarioDataJpaRepository;
+    private final UsuarioMapper usuarioMapper;
+
+    @Override
+    public Usuario guardarUsuario(Usuario usuario) {
+        UsuarioData usuarioDataGuardar = usuarioMapper.tousuarioData(usuario);
+
+        return usuarioMapper.toUsuario(usuarioDataJpaRepository.save(usuarioDataGuardar));
+    }
+
+    @Override
+    public Usuario buscarUsuarioPorCc(String cedula) {
+
+        UsuarioData usarioEncontrado = usuarioDataJpaRepository.findById(cedula).orElse(null);
+
+        if(usarioEncontrado == null){
+            return null;
+        }
+
+        return usuarioMapper.toUsuario(usarioEncontrado);
+    }
+
+
+    @Override
+    public void eliminarUsuarioPorCc(String cedula) {
+        usuarioDataJpaRepository.deleteById(cedula);
+    }
+
+    @Override
+    public Usuario buscarPorCorreo(String correo) {
+        return usuarioDataJpaRepository.findByCorreo(correo).map(usuarioMapper::toUsuario).orElse(null);
+    }
+
+}
---
+++ auth/src/main/java/com/surtiana/auth/infraestructure/driver_adapters/jpa_repository/UsuarioDataJpaRepository.java
@@ -0,0 +1,12 @@
+package com.surtiana.auth.infraestructure.driver_adapters.jpa_repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UsuarioDataJpaRepository extends JpaRepository<UsuarioData, String> {
+
+    Optional<UsuarioData> findByCorreo(String correo);
+
+}
