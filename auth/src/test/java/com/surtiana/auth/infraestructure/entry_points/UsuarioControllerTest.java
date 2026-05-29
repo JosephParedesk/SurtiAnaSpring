f9b26cdf4f0753800f3a08de59c8ed2fdd03dd73
Joseph Paredes <alejitoparedes2006@gmail.com>
2026-05-29 11:03:51 -0500

    Corregi error de Versiones

--- auth/src/test/java/com/surtiana/auth/infraestructure/entry_points/UsuarioControllerTest.java
+++ auth/src/test/java/com/surtiana/auth/infraestructure/entry_points/UsuarioControllerTest.java
@@ -1,45 +1,60 @@
 package com.surtiana.auth.infraestructure.entry_points;
 
-import org.springframework.boot.test.json.JacksonTester;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.surtiana.auth.domain.model.Usuario;
 import com.surtiana.auth.domain.usecase.UsuarioUseCase;
 import com.surtiana.auth.infraestructure.driver_adapters.jpa_repository.UsuarioData;
 import com.surtiana.auth.infraestructure.mapper.UsuarioMapper;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import org.springframework.http.MediaType;
-
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@WebMvcTest(UsuarioController.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
 class UsuarioControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Mock
     private UsuarioUseCase usuarioUseCase;
 
-    @MockBean
+    @Mock
     private UsuarioMapper usuarioMapper;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    @InjectMocks
+    private UsuarioController usuarioController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(usuarioController)
+                .build();
+    }
 
     @Test
     void deberiaGuardarUsuario() throws Exception {
 
-        // Arrange
         UsuarioData usuarioData = new UsuarioData();
         usuarioData.setCedula("123");
         usuarioData.setCorreo("johan@gmail.com");
@@ -55,7 +70,6 @@ class UsuarioControllerTest {
         when(usuarioUseCase.guardarUsuario(usuario))
                 .thenReturn(usuario);
 
-        // Act & Assert
         mockMvc.perform(post("/api/surtiana/usuario/save")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(usuarioData)))
@@ -68,7 +82,6 @@ class UsuarioControllerTest {
     @Test
     void deberiaBuscarUsuarioPorCedula() throws Exception {
 
-        // Arrange
         Usuario usuario = new Usuario();
         usuario.setCedula("123");
         usuario.setCorreo("johan@gmail.com");
@@ -76,7 +89,6 @@ class UsuarioControllerTest {
         when(usuarioUseCase.buscarUsuarioPorCc("123"))
                 .thenReturn(usuario);
 
-        // Act & Assert
         mockMvc.perform(get("/api/surtiana/usuario/buscar/123"))
                 .andExpect(status().isOk());
 
@@ -86,7 +98,6 @@ class UsuarioControllerTest {
     @Test
     void deberiaEliminarUsuario() throws Exception {
 
-        // Act & Assert
         mockMvc.perform(delete("/api/surtiana/usuario/eliminar/123"))
                 .andExpect(status().isNoContent());
 
@@ -96,7 +107,6 @@ class UsuarioControllerTest {
     @Test
     void deberiaHacerLogin() throws Exception {
 
-        // Arrange
         UsuarioData usuarioData = new UsuarioData();
         usuarioData.setCorreo("johan@gmail.com");
         usuarioData.setContrasena("123456");
@@ -106,7 +116,6 @@ class UsuarioControllerTest {
                 usuarioData.getContrasena()
         )).thenReturn("TOKEN_VALIDO");
 
-        // Act & Assert
         mockMvc.perform(post("/api/surtiana/usuario/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(usuarioData)))
