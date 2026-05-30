b85b87eca40435a4b6d6abdf3d0ed13ef316a8d6
jstivenmusic17-rgb <jstivenmusic17@gmail.com>
2026-05-30 00:35:48 -0500

    modulo de catalogo completo con 99% de cobertura

--- catalogo/src/test/java/com/surtiana/catalogo/application/config/UseCaseConfigTest.java
+++ catalogo/src/test/java/com/surtiana/catalogo/application/config/UseCaseConfigTest.java
@@ -1,30 +1,46 @@
 package com.surtiana.catalogo.application.config;
 
 import com.surtiana.catalogo.domain.model.gateway.ProductoGateway;
+import com.surtiana.catalogo.domain.model.gateway.ReciboGateway;
+import com.surtiana.catalogo.domain.model.gateway.VentaGateway;
 import com.surtiana.catalogo.domain.usecase.ProductoUseCase;
+import com.surtiana.catalogo.domain.usecase.VentaUseCase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 class UseCaseConfigTest {
 
     private UseCaseConfig useCaseConfig;
-    private ProductoGateway productoGateway;
 
     @BeforeEach
     void setUp() {
         useCaseConfig = new UseCaseConfig();
-        productoGateway = mock(ProductoGateway.class);
     }
 
     @Test
-    void productoUseCase_ShouldReturnInstance() {
-        // Arrange (Configurado en el setUp)
+    void productoUseCase_Exitoso_RetornaInstancia() {
+        // Arrange
+        ProductoGateway productoGateway = mock(ProductoGateway.class);
 
         // Act
         ProductoUseCase resultado = useCaseConfig.productoUseCase(productoGateway);
+
+        // Assert
+        assertNotNull(resultado);
+    }
+
+    @Test
+    void ventaUseCase_Exitoso_RetornaInstancia() {
+        // Arrange
+        VentaGateway ventaGateway = mock(VentaGateway.class);
+        ProductoGateway productoGateway = mock(ProductoGateway.class);
+        ReciboGateway reciboGateway = mock(ReciboGateway.class);
+
+        // Act
+        VentaUseCase resultado = useCaseConfig.ventaUseCase(ventaGateway, productoGateway, reciboGateway);
 
         // Assert
         assertNotNull(resultado);
